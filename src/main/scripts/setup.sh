f5ed7764a9f10faa5a06d4fcfbdc6989fe3bb5fd
Warfront1 <1920038+Warfront1@users.noreply.github.com>
2025-08-17 16:14:00 -0400

    Update RREADME and allow for restarts. (#3)

--- src/main/scripts/setup.sh
+++ src/main/scripts/setup.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+LOCK_FILE="/.setup_done"
+
+# This command needs to run every time the container starts
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+
+# Exit if setup has already been completed
+if [ -f "$LOCK_FILE" ]; then
+    echo "Setup already complete. Skipping."
+    exit 0
+fi
+
+echo "Performing first-time setup..."
 SERVER_NAME="${CA_SERVER_NAME:=myservername}"
 CA_KEY_PASSWORD="${CA_KEY_PASSWORD:=test}"
 CLIENT_NAME="${CLIENT_NAME:=client}"
@@ -13,4 +25,6 @@ PUBIP="$(get_public_ip)"
 # Generate inline client profile (.ovpn) directly into /client_credentials/
 /scripts/generate_client_ovpn.sh --server-host "$PUBIP" --clientname "$CLIENT_NAME" --verify-x509-name "$SERVER_NAME" --output "/client_credentials/${CLIENT_NAME}.ovpn"
 
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+# Create the lock file to indicate setup is complete
+touch "$LOCK_FILE"
+echo "First-time setup complete."
